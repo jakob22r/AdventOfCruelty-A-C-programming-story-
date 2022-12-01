b152843e700bfafb375f63709931e762b82a0917
jakob22r <92209540+jakob22r@users.noreply.github.com>
2022-12-01 21:35:21 +0100

    C# Solution Added for part 1

---
+++ Day 1/part1-cs/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] arr = System.IO.File.ReadAllLines("input.txt");
+
+int globalSum = 0;
+int localSum = 0;
+
+foreach (var item in arr)
+{
+    if (string.IsNullOrEmpty(item)) {
+        if (localSum > globalSum) {
+            globalSum = localSum;
+        }
+        localSum = 0;
+    } else {
+        int res = 0;
+        Int32.TryParse(item, out res);
+        localSum = localSum + res;
+    }
+    System.Console.WriteLine("Result {0}", globalSum);
+
+}
+
